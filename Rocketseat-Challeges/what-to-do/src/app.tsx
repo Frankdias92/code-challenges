70f0db1a5186a7717841d792f30f04f4d7c9d980
Frankdias92 <frankmcdias@gmail.com>
2024-02-11 23:56:45 -0300

    create dialog on task

--- Rocketseat-Challeges/what-to-do/src/app.tsx
+++ Rocketseat-Challeges/what-to-do/src/app.tsx
@@ -3,18 +3,23 @@ import { NewNoteTask } from "./components/new-note-task";
 import { NoteTask } from "./components/note-task";
 
 
+const task = {
+  title: 'Task title',
+  content: 'task comment'
+}
+
+
 export function App() {
   return (
-    <section className="flex flex-col">
+    <section className="flex flex-col h-[100vh]">
       <Header />
 
-      <div className="mx-auto max-w-6xl space-y-4 flex flex-col">
+      <div className="mx-auto max-w-6xl gap-y-4 flex flex-col">
         <NewNoteTask />
 
         <div className="h-px bg-stone-700"/>
 
-        <NoteTask />
-        <NoteTask />
+        <NoteTask task={task} />
       </div>
     </section>
   )
--- Rocketseat-Challeges/what-to-do/src/components/note-task.tsx
+++ Rocketseat-Challeges/what-to-do/src/components/note-task.tsx
@@ -1,33 +1,75 @@
+import * as Dialog from '@radix-ui/react-dialog'
+import { Circle, Trash2, X } from 'lucide-react'
 
+interface NoteTaskProps {
+    task: {
+        title: string
+        content: string
+    }
+}
 
-export function NoteTask() {
+export function NoteTask({ task }: NoteTaskProps) {
     return (
 
-        <button className="flex flex-row w-full justify-between items-start
-        rounded-md bg-stone-700 py-5 px-7 h-20 relative overflow-hidden
-        hover:ring-2 hover:ring-stone-500 text-left
-        outline-none focus-visible:ring-2 focus-visible:ring-lime-500">
-            <div className="flex flex-col pb-2">
-              <div className="flex items-start gap-8 pr-4">
-                <h1>x</h1>
+        <Dialog.Root>
+            <Dialog.Trigger className="flex flex-row w-full justify-between items-start
+            rounded-md bg-stone-700 py-5 px-7 h-20 relative overflow-hidden
+            hover:ring-2 hover:ring-stone-500 text-left
+            outline-none focus-visible:ring-2 focus-visible:ring-lime-500">
+                <div className="flex flex-col pb-2">
+                    <div className="flex items-start gap-8 pr-4">
+                        <Circle size={22} className='text-blue-400'/>
 
-                <p className="w-[450px] ">
-                  Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-                </p>
-              </div>
+                        <p className="w-[450px]">
+                            {task.title}
+                        </p>
+                    </div>
 
-              <div className="h-8 overflow-hidden">
-              <p className="text-stone-500 w-[450px] pl-10 text-sm font-medium leading-6">test coment - Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptas repudiandae sit ipsam. Totam, autem amet nostrum sapiente neque fugit nisi ullam recusandae, ad doloribus consequuntur doloremque ex libero. Enim, et.</p>
-              </div>
-            </div>
+                    <div className="h-8 overflow-hidden">
+                        <p className="text-stone-500 w-[450px] pl-10 text-sm font-medium leading-6">
+                            {task.content}
+                        </p>
+                    </div>
+                </div>
 
-            <button className="text-red-500 rounded-md
-            outline-none focus-visible:ring-2 focus-visible:ring-red-400">
-                empty
-            </button>
+                <button className="text-red-500 rounded-md
+                outline-none focus-visible:ring-2 focus-visible:ring-red-400
+                hover:text-red-400 duration-75">
+                    <Trash2 />
+                </button>
 
-            <div className="absolute bottom-0 left-0 right-0 h-1/3 bg-gradient-to-t from-black/45 to-black/0 pointer-events-none"/>
-        </button>
+                <div className="absolute bottom-0 left-0 right-0 h-1/3 bg-gradient-to-t from-black/45 to-black/0 pointer-events-none"/>
+            </Dialog.Trigger>
+
+
+            <Dialog.Portal>
+                <Dialog.Overlay className='inset-0 fixed bg-black/60'/> 
+                
+                <Dialog.Content
+                    className='z-10 left-1/2 top-1/2 absolute
+                    -translate-x-1/2 -translate-y-1/2 max-w-[734px] w-full h-[45vh]
+                    bg-stone-700 rounded-md flex flex-col outline-none overflow-hidden'>
+
+                    <Dialog.Close className='absolute right-2 top-2 bg-stone-800 rounded-full p-1 text-stone-400 hover:text-red-400'>
+                        <X size={20}/>
+                    </Dialog.Close>
+                    
+                    <div className='flex flex-1 flex-col gap-3 p-5'>
+                        <span>{task.title}</span>
+                    </div>
+                        
+
+                    <button
+                        type='button'
+                        className='w-full bg-stone-800 py-4 font-medium text-center to-stone-500 outline-none group'
+                    >
+                        Deseja <span className='text-red-500 group-hover:underline'>apagar tarefa</span>?
+                    </button>
+                </Dialog.Content>
+                
+            </Dialog.Portal>
+            
+        </Dialog.Root>
 
     )
 }
