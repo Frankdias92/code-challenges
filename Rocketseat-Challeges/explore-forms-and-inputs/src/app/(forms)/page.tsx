6e16dc662eb22e5b2154bfd7e938a02b4e42f152
Franklin Macedo Dias <frankmcdias@gmail.com>
2024-03-28 22:53:40 -0300

    first commit on explore-forms-and-inputs

---
+++ Rocketseat-Challeges/explore-forms-and-inputs/src/app/(forms)/page.tsx
@@ -0,0 +1,12 @@
+import { Forms } from "@/components/forms";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="bg-slate-900 text-slate-200">
+      <h1>Forms</h1>
+
+      <Forms />
+    </div>
+  );
+}
---
+++ Rocketseat-Challeges/explore-forms-and-inputs/src/components/forms.tsx
@@ -0,0 +1,9 @@
+
+
+export function Forms() {
+    return (
+        <div>
+            <h1>hello, Im a form</h1>
+        </div>
+    )
+}
